009f95e2142e18b18dca18eac2e22bb61e5128ef
James Orozco <jamesorozcoh@gmail.com>
2024-03-19 09:55:42 -0500

    clas 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import Footer from "./Footer.jsx"
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  let name = "James"
-
-  const sum = () => {
-    console.log(5+2)
-  }
   return (
     <div>
-    <h1>hello {name}</h1>
-    <h2>How you doing?</h2>
-    <button onClick={sum}>Sumar</button>
-    <Footer />
+      <Navbar />
+      <Footer />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red", fontSize: "1rem", marginTop: "10px" }}>
+        Home
+      </h1>
+      <h2>Hola</h2>
+      <img
+        src="https://res.cloudinary.com/dsrxguglp/image/upload/v1710859689/calzado_j2km8g.webp"
+        alt="shoes"
+      />
+    </div>
+  );
+};
+
+export default Home;
